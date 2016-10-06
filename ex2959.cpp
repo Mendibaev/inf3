90788aefdc350b3b6b2ec3a61b2a3a9b0022c1a4
Mendibaev <mendibaev98@gmail.com>
2016-10-06 10:15:53 +0600

    Add files via upload

---
+++ ex2959.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+int a;
+cin>>a;
+if (a>0) 
+{cout<<"1";}
+if (a<0) 
+{cout<<"-1";
+}
+if (a==0)
+{
+cout<<"0";}
+return 0;
+}
